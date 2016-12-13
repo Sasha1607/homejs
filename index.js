1b4ed61210512f841cd0258fd5845ade4328e89e
Sasha Atl <sasha1998f@mail.ru>
2016-12-13 14:29:34 +0000

    int

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-var first = Number (prompt('first'));
-var second = Number (prompt('second'));
-var third = Number (prompt('third'));
+var first = Number (prompt('first ax^2 + bx +c = 0'));
+var second = Number (prompt('second ax^2 + bx +c = 0'));
+var third = Number (prompt('third ax^2 + bx +c = 0'));
 var disk = form(first, second, third);
 function form(a, b, c) {
     return b * b - 4 * a * c; 
 }
 
 function diskreminant(disk) {
-    if (disk >= 1)
+    if (disk > 0)
     {
         return Math.sqrt(disk);
     }   else {
@@ -16,5 +16,13 @@ function diskreminant(disk) {
         }
     }
 var result = diskreminant(disk);
+function x1 (a, b ,disk) {
+    var x1 = - b - Math.sqrt(disk) / (2 * a);
+}
+function x2 (a, b ,disk) {
+    var x2 = - b + Math.sqrt(disk) / (2 * a);
+}
+document.write(x1);
+document.write(x2);
 document.write('корінь з дискримінанту = ');
 document.write(result);
